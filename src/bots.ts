dfb763568aa8e8058f42cda01e3d0e785ac87a22
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-05-12 22:58:38 +0530

    merge fix conflict

--- src/bots.ts
+++ src/bots.ts
@@ -5,13 +5,16 @@ const bots = new Map<string, Bot>();
 export function getBot(token: string) {
     let bot = bots.get(token);
     if (!bot) {
-        bot = new Bot(token, {
-            client: {
-                // We accept the drawback of webhook replies for typing status.
-                canUseWebhookReply: (method) => method === "sendChatAction",
-            },
-        });
-        bots.set(token, bot);
+        try {
+            bot = new Bot(token, {
+                client: {
+                    // We accept the drawback of webhook replies for typing status.
+                    canUseWebhookReply: (method) => method === "sendChatAction",
+                },
+            });
+            bots.set(token, bot);
+        }
+        catch (e) {}
     }
     return bot;
 }
